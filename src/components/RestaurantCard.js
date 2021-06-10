f5a27e74a3e4bb9608de92f27b2a61e81953d925
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-06-09 20:20:51 -0500

    looks good, need to come back and style

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -21,22 +21,22 @@ function RestaurantCard({
         <CardTitle className="text-center" tag="h3">{name}</CardTitle>
         <CardSubtitle tag="h6" className="mb-2 text-muted">{description}</CardSubtitle>
         <div className="mt-auto">
-          <div className="links-container">
-            <div className="links-name-container">
+          <div className="details-container">
+            <div className="details-name-container">
               <h6>Cuisine</h6>
               <CardSubtitle tag="h6" className="mb-2 text-muted">{cuisineType}</CardSubtitle>
             </div>
-            <div className="links-name-container">
+            <div className="details-name-container">
               <h6>Neighborhood</h6>
               <CardSubtitle tag="h6" className="mb-2 text-muted">{neighborhood}</CardSubtitle>
               </div>
             </div>
-          <div className="links-container">
-            <div className="links-name-container">
+          <div className="details-container">
+            <div className="details-name-container">
             <CardSubtitle tag="h6" className="mb-2 text-muted">Website</CardSubtitle>
               <a href={websiteLink} target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/ios/100/000000/restaurant-menu.png" className="links-icons"/></a>
             </div>
-            <div className="links-name-container">
+            <div className="details-name-container">
               <CardSubtitle tag="h6" className="mb-2 text-muted">Reservations</CardSubtitle>
               <a href={reservationLink} target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/ios/100/000000/reservation.png" className="links-icons"/></a>
             </div>
