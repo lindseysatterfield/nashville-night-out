bef909364786765439458b9d890f45c2215f6dd0
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-06-11 18:52:35 -0500

    code cleanup

--- src/helpers/views/AddRestaurant.js
+++ src/helpers/views/AddRestaurant.js
@@ -3,20 +3,16 @@ import PropTypes from 'prop-types';
 import { Container } from 'reactstrap';
 import RestaurantForm from '../../components/RestaurantForm';
 
-export default function AddRestaurant({
-  user, setRestaurants, favorites, setFavorites
-}) {
+export default function AddRestaurant({ user, setRestaurants }) {
   return (
     <Container className="add-restaurant-container">
       <h1 className="my-3">Add Restaurant</h1>
-      <RestaurantForm user={user} setRestaurants={setRestaurants} favorites={favorites} setFavorites={setFavorites} />
+      <RestaurantForm user={user} setRestaurants={setRestaurants} />
     </Container>
   );
 }
 
 AddRestaurant.propTypes = {
   user: PropTypes.any,
-  setRestaurants: PropTypes.func,
-  favorites: PropTypes.array,
-  setFavorites: PropTypes.func
+  setRestaurants: PropTypes.func
 };
