1e3ac4ea08d091fa887e9af1560fb9068c490db2
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-06-20 16:46:47 -0500

    getting ainfo back from API

--- src/helpers/Yelp/YelpData.js
+++ src/helpers/Yelp/YelpData.js
@@ -1,28 +1,12 @@
 import axios from 'axios';
 import { YelpConfig } from '../apiKeys';
 
-const dbUrl = YelpConfig.databaseURL; // https://api.yelp.com/v3/
+const dbUrl = YelpConfig.databaseURL;
 const yelpKey = YelpConfig.YelpApiKey;
-const CORS = 'https://cors-anywhere.herokuapp.com/';
-
-// const yelpREST = axios.create({
-//   dbUrl,
-//   headers: {
-//     Authorization: `Bearer ${yelpKey}`,
-//     'Content-type': 'application/json',
-//   },
-// });
-
-// const getYelpRestaurantInfo = () => new Promise((resolve, reject) => {
-//   yelpREST('businesses/search?term=japanese&location=nashville&limit=5')
-//     .then((response) => {
-//       resolve(response);
-//     })
-//     .catch((error) => reject(error));
-// });
+const cors = 'https://cors-anywhere.herokuapp.com/';
 
 const getYelpRestaurantInfo = () => new Promise((resolve, reject) => {
-  axios.get(`${CORS}${dbUrl}businesses/search?term=japanese&location=nashville&limit=5`, {
+  axios.get(`${cors}${dbUrl}businesses/search?term=japanese&location=nashville&limit=5`, {
     headers: {
       Authorization: `Bearer ${yelpKey}`,
       'Content-type': 'application/json'
